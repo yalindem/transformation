147ae3bbd3686f877a401f986f648f2e8c3e331f
yalin:turtle [ubuntu-22.04] <yalindemir@outlook.de>
2026-02-21 14:12:14 +0100

    add a point in front of the camera1

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def run_simulation():
     # 1. Giriş Değerleri
     drone_pos = [5, 0, 8] # in world coordinates
     drone_ori = [0, 0, 45] # Roll, Pitch, Yaw
-    point_drone = [3, 0, 0] # Drone'un 3m önü
+    point_A_drone = [3, 0, 0] # Drone'un 3m önü
+    point_B_camea1 = [0, 0, 2] # camera 1 frameine göre bir cisim
 
     camera1_pos_drone = [0.2, -0.05, 0.1]
     camera1_ori_drone = [-90, 0, -90]  
@@ -15,13 +16,15 @@ def run_simulation():
 
     # 2. Hesaplama (Logic)
     T_world_drone = create_full_transformation_matrix(*drone_pos, *drone_ori)
-    point_world = transform_point(T_world_drone, point_drone)
+    point_A_world = transform_point(T_world_drone, point_A_drone)
 
     T_drone_camera1 = create_full_transformation_matrix(*camera1_pos_drone, *camera1_ori_drone)
     T_drone_camera2 = create_full_transformation_matrix(*camera2_pos_drone, *camera2_ori_drone)
     
     T_world_camera1 = T_world_drone @ T_drone_camera1
-    T_world_camera2 = T_world_drone @ T_drone_camera1
+    T_world_camera2 = T_world_drone @ T_drone_camera2
+
+    point_B_world = transform_point(T_world_camera1, point_B_camea1)
 
     frames = [
         {'matrix': T_world_drone,   'label': 'Drone (B)'},
@@ -31,8 +34,9 @@ def run_simulation():
 
     # Çizilecek Noktalar Listesi
     points = [
-        {'coord': point_world, 'label': 'Target P', 'color': 'black'},
-        {'coord': [2, 3, 0],   'label': 'Obstacle', 'color': 'red'}
+        {'coord': point_A_world, 'label': 'Target A', 'color': 'black'},
+        {'coord': [2, 3, 0],   'label': 'Obstacle', 'color': 'red'},
+        {'coord': point_B_world, 'label': 'Target B', 'color': 'black'}
     ]
 
     # Fonksiyonu çağır (Parametre kalabalığı bitti!)
